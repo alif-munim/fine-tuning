23985561fda01f86185bf58f60d19c922e3ecbaf
Alif Munim <alif.munim@torontomu.ca>
2023-12-07 11:59:54 -0500

    Add qLoRA training scripts

---
+++ segment/save_model.py
@@ -0,0 +1,41 @@
+import os
+import torch
+from datasets import load_dataset, Dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    TrainingArguments,
+    pipeline,
+    logging,
+)
+from peft import LoraConfig, PeftModel
+from trl import SFTTrainer
+
+model_name = "meta-llama/Llama-2-7b-hf"
+dataset_name = "timdettmers/openassistant-guanaco"
+new_model = "llama-2-7b-guanaco"
+merged_model = "llama-2-7b-guanaco-merged"
+
+
+
+# Reload model in FP16 and merge it with LoRA weights
+base_model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    low_cpu_mem_usage=True,
+    return_dict=True,
+    torch_dtype=torch.float16,
+    # device_map=device_map,
+)
+model = PeftModel.from_pretrained(base_model, new_model)
+model = model.merge_and_unload()
+
+# Reload tokenizer to save it
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+
+
+model.save_pretrained(merged_model, use_temp_dir=False)
+tokenizer.save_pretrained(merged_model, use_temp_dir=False)
